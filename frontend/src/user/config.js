925d2c9c5a588ea29620e49a6c31517fff651990
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-06-07 23:36:46 +0530

    Update config.js

--- frontend/src/user/config.js
+++ frontend/src/user/config.js
@@ -1,5 +1,3 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || `http://${window.location.hostname}:8000`;
+  API_URL: process.env.REACT_APP_API_URL || `https://${window.location.hostname}:8000`
 };
-
-export default config;
