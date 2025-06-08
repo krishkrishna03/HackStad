c87d1049b59003a48435fb38cb9d246ff4da2716
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-06-08 21:36:45 +0530

    Update Home.js

--- frontend/src/Home/Home.js
+++ frontend/src/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Loading from "./Home/Loading";
-import Home from "./Home/Main";
+import Home from "./Home/Home";
 import SignupSelection from './Home/SignupSelection';
 import CollegeRegister from './College/Collegeregister';
 import CollegeDashboard from './College/CollegeDashboard';
