170f150f729aa76593ede47c78767d4545cf64a4
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-06-10 12:59:17 +0530

    Update LeaderBoard.js

--- frontend/src/team/components/LeaderBoard.js
+++ frontend/src/team/components/LeaderBoard.js
@@ -13,8 +13,10 @@ export default function Leaderboard() {
   const hackathonId = queryParams.get('hackathon_id');
 
   const getBaseURL = () => {
-    const ip = window.location.hostname;
-    return `http://${ip}:8000`;
+    if (process.env.REACT_APP_API_URL) {
+      return process.env.REACT_APP_API_URL;
+    }
+    return 'https://hackstad-0nqg.onrender.com';
   };
 
   useEffect(() => {
