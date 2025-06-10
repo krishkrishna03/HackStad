530b6fa8a7c6797a2546f503dbab9630dddf422e
patnala sri krishna sai <96357392+krishkrishna03@users.noreply.github.com>
2025-06-10 13:01:32 +0530

    Update TimeTable.js

--- frontend/src/team/components/TimeTable.js
+++ frontend/src/team/components/TimeTable.js
@@ -10,8 +10,10 @@ export default function SubmitProject() {
   const [problemStatementsUrl, setProblemStatementsUrl] = useState(null);
 
   const getBaseURL = () => {
-    const ip = window.location.hostname;
-    return `http://${ip}:8000`;
+    if (process.env.REACT_APP_API_URL) {
+      return process.env.REACT_APP_API_URL;
+    }
+    return 'https://hackstad-0nqg.onrender.com';
   };
 
   useEffect(() => {
